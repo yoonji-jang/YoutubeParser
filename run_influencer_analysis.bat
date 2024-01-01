4925a341d9d6d87f6144014d7eb20ddf29196a59
yoonji jang <jjyjjjjjjj@gmail.com>
2024-01-01 23:33:31 +0900

    remove instagram description from run_influencer_analysis.bat

--- run_influencer_analysis.bat
+++ run_influencer_analysis.bat
@@ -1,7 +1,5 @@
 :: --yc (youtube channel)
 ::--yv (youtube video)
-::--ic (instagram channel)
-::--ip (instagram post)
 
-:: .\InfluencerAnalysis --yc --yv --ic --ip
-cmd/k YoutubeParser.exe --ia -input input_influencer.txt --yc
+:: .\InfluencerAnalysis --yc --yv
+cmd/k YoutubeParser.exe --ia -input input_influencer.txt --yc --yv
