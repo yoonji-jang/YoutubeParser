d3d39e031abcddf8d84ac76fedeff4d990b327c2
yoonji jang <jjyjjjjjjj@gmail.com>
2024-07-07 00:31:11 +0900

    add excel_func.py for excel functions

---
+++ excel_func.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.styles import Font
+
+# make output
+def make_excel(df_datas, output_path):
+    wb = Workbook()
+    ws = wb.active
+    df_just_video = pd.DataFrame(df_datas)
+    ext = os.path.splitext(output_path)[1]
+    if ext == ".csv":
+        df_just_video.to_csv(output_path, encoding='utf-8-sig', index=False)
+        df_xl = pd.read_csv(output_path)
+    else:
+        df_just_video.to_excel(output_path, encoding='utf-8-sig', index=False)
+        df_xl = pd.read_excel(output_path)
+
+    for row in dataframe_to_rows(df_xl, index=False, header=True):
+        ws.append(row)
+    for row in ws.iter_rows(min_row=2, max_col=len(df_xl.columns), max_row=len(df_xl) + 1):
+        for cell in row:
+            if "http" in str(cell.value):
+                cell.font = Font(underline="single", color="0563C1")
+                cell.hyperlink = cell.value
+    wb.save(output_path)
--- video_analysis.py
+++ video_analysis.py
@@ -1,14 +1,9 @@
 import time
-import pandas as pd
-import os
 from video_input_parser import parse_input_data
 from driver import *
 from youtube_parser import run_search, get_video_data
 from youtube_video_analysis import run_VideoAnalysis
-from openpyxl import Workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
-from openpyxl.styles import Font
-import re
+from excel_func import make_excel
 
 def run_video_analysis(args):
     print("[Info] Run Video Analysis")
@@ -41,25 +36,3 @@ def read_input(input_text):
     input_file.close()
     print("[Info] Done reading : " + input_text)
     return parse_input_data(input_data)
-
-# make output
-def make_excel(df_datas, output_path):
-    wb = Workbook()
-    ws = wb.active
-    df_just_video = pd.DataFrame(df_datas)
-    ext = os.path.splitext(output_path)[1]
-    if ext == ".csv":
-        df_just_video.to_csv(output_path, encoding='utf-8-sig', index=False)
-        df_xl = pd.read_csv(output_path)
-    else:
-        df_just_video.to_excel(output_path, encoding='utf-8-sig', index=False)
-        df_xl = pd.read_excel(output_path)
-
-    for row in dataframe_to_rows(df_xl, index=False, header=True):
-        ws.append(row)
-    for row in ws.iter_rows(min_row=2, max_col=len(df_xl.columns), max_row=len(df_xl) + 1):
-        for cell in row:
-            if "http" in str(cell.value):
-                cell.font = Font(underline="single", color="0563C1")
-                cell.hyperlink = cell.value
-    wb.save(output_path)
