58d589a4d00e8621fefbda4f1688984b01027558
yoonji jang <jjyjjjjjjj@gmail.com>
2024-07-07 10:57:54 +0900

    add coolenjoy function

--- tech_community_parser.py
+++ tech_community_parser.py
@@ -27,6 +27,8 @@ def run_search(driver, keyword, period_date_start, platform_name):
 
     if platform_name == "quasarzone":
         return search_quasarzone(platform, driver, keyword, period_date_start)
+    elif platform_name == "coolenjoy":
+        return search_coolenjoy(platform, driver, keyword, period_date_start)
 
 
 
@@ -54,13 +56,63 @@ def search_quasarzone(platform, driver, keyword, period_date_start):
                 # date
                 date_elem = post.find('p', class_='time')
                 if date_elem == None:
-                    break
+                    continue
                 date_str = date_elem.text.strip()
                 date_post = time.strptime(date_str, "%Y-%m-%d")
 
                 title_elem = post.find('p', class_="title")
                 if title_elem == None:
-                    break
+                    continue
+                title = title_elem.text.strip()
+                link_href = post.find('a')["href"]
+                link = urljoin(url, link_href)
+                if period_date_start <= date_post:
+                    elem = {"url": link, "date": date_str, "title": title}
+                    post_list.append(elem)
+            page_num += 1
+        except Exception as exception:
+            print("[Warning] " + str(exception))
+            continue
+
+    return post_list
+
+
+
+def search_coolenjoy(platform, driver, keyword, period_date_start):
+    print("[Info] Run search_coolenjoy")
+    post_list = []
+    date_post = period_date_start
+    page_num = 1
+    while True:
+        url = platform["url"] + keyword.replace(" ", "+") + platform["page"] + str(page_num)
+        print("[Info] search : " + url)
+        driver.get(url)
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+
+        if period_date_start > date_post:
+            print("[Info] Meet the date. Stop search!!! : ")
+            break
+
+        try:
+            posts = soup.find_all('ul', class_='na-table d-md-table w-100')
+            if len(posts) == 0:
+                break
+            for post in posts:
+                # date
+                sr_elem = post.find('div', class_='float-left text-muted')
+                if sr_elem == None:
+                    sr_elem = post.find('div', class_='float-left float-md-none d-md-table-cell nw-6 nw-md-auto f-sm font-weight-normal py-md-2 pr-md-1')
+                    if sr_elem == None:
+                        continue
+
+                text_contents = [item.strip() for item in sr_elem.contents if isinstance(item, str)]
+                date_str = text_contents[2]
+                date_post = time.strptime(date_str, "%y.%m.%d")
+
+                title_elem = post.find('div', class_="na-item")
+                if title_elem == None:
+                    continue
                 title = title_elem.text.strip()
                 link_href = post.find('a')["href"]
                 link = urljoin(url, link_href)
