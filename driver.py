d8da30f06935f7824ba1b6b6adc927e52521c1d5
yoonji jang <jjyjjjjjjj@gmail.com>
2024-07-08 21:02:24 +0900

    support latest chrome version

--- driver.py
+++ driver.py
@@ -1,11 +1,17 @@
-from selenium import webdriver as wd
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+release = "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
+version = requests.get(release).text
 # define environment
-webdriver_options = wd.ChromeOptions()
-webdriver_options.add_argument("headless")
-webdriver_options.add_argument("lang=ko")
-webdriver_options.add_argument('--log-level=3')
-driver = wd.Chrome(service=Service(ChromeDriverManager().install()), options=webdriver_options)
-driver_video = wd.Chrome(service=Service(ChromeDriverManager().install()), options=webdriver_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
+chrome_options.add_argument("lang=ko")
+chrome_options.add_argument('--log-level=3')
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+driver = webdriver.Chrome(options=chrome_options)
+driver_video = webdriver.Chrome(options=chrome_options)
