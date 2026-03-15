6f5224e29c077c2be3a630350a7c2988141c1eb3
yoonji jang <jjyjjjjjjj@gmail.com>
2026-03-15 14:16:10 +0900

    skip setting hyperlink and version up to 7.6

--- excel_func.py
+++ excel_func.py
@@ -15,14 +15,14 @@ def make_excel(df_datas, output_path):
         df_xl = pd.read_csv(output_path)
     else:
         df_just_video.to_excel(output_path, encoding='utf-8-sig', index=False)
-        df_xl = pd.read_excel(output_path)
+    #     df_xl = pd.read_excel(output_path)
 
-    for row in dataframe_to_rows(df_xl, index=False, header=True):
-        ws.append(row)
-    for row in ws.iter_rows(min_row=2, max_col=len(df_xl.columns), max_row=len(df_xl) + 1):
-        for cell in row:
-            if "http" in str(cell.value):
-                cell.font = Font(underline="single", color="0563C1")
-                cell.hyperlink = cell.value
-    wb.save(output_path)
+    # for row in dataframe_to_rows(df_xl, index=False, header=True):
+    #     ws.append(row)
+    # for row in ws.iter_rows(min_row=2, max_col=len(df_xl.columns), max_row=len(df_xl) + 1):
+    #     for cell in row:
+    #         if "http" in str(cell.value):
+    #             cell.font = Font(underline="single", color="0563C1")
+    #             cell.hyperlink = cell.value
+    # wb.save(output_path)
     print("[Info] Saved Output Excel : " + output_path)
